cf0a2468b621ac49a471dd3500aeacde31f87bf6
Artem Deynega <artemdeynega892@gmail.com>
2021-06-25 20:13:12 +0300

    32

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import "./styles/main.scss";
-import { test } from "./utils/utils";
+// import './styles/main.scss';
+// import { test } from './utils/utils';
 
-test();
+// test();
 
-class Test {
-  constructor(item) {
-    this.item = item;
-  }
-  consoleItem() {
-    console.log(this.item);
-  }
-}
+// class Test {
+//     constructor(item) {
+//         this.item = item;
+//     }
+//     consoleItem() {
+//         console.log(this.item);
+//     }
+// }
 
-const test1 = new Test("Привет");
+// const test1 = new Test('Привет');
 
-test1.consoleItem();
+// test1.consoleItem();
+const menuData = {
+    title: 'Eat it createElement, templates rule!',
+    items: ['Handlebars', 'LoDash', 'Pug', 'EJS', 'lit-html'],
+};
+
+const source = document
+    .querySelector('#menu-template')
+    .innerHTML.trim();
+
+const template = Handlebars.compile(source);
+
+/*
+  Создадим переменную markup, в которую запишем 
+  результат работы функции-шаблона template
+*/
+
+const markup = template(menuData);
+
+console.log(markup);
+
+/*
+  Пример выше похож на операцию «поиск-и-замена»: шаблон просто 
+  заменил {{title}} на значение свойства menuData.title. 
+  После чего в результат попал сначала текст <ul>, 
+  потом выполнился {{#each items}}, который последовательно 
+  сгенерировал элементы списка, и затем список был закрыт </ul>.
+*/
+
+/*
+  В js выберем контейнер по id, после чего используя 
+  свойство innerHTML присвоим туда нашу строку.
+*/
+const container = document.querySelector('#munu-container');
+
+container.innerHTML = markup;
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.js$/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.handlebars$/,
-        loader: "handlebars-loader",
-      },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.js$/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.handlebars$/,
+                loader: 'handlebars-loader',
+            },
+            {
+                test: /\.hbs$/,
+                exlude: /node_modules/,
+                use: 'handlebars-loader',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
-    }),
-  ],
 };
