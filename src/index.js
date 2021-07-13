a1eccd6b088f53c4a861af1eba315bf3bb415b43
Artem Deynega <artemdeynega892@gmail.com>
2021-07-13 17:33:20 +0300

    Промисы, then, catch, finally

--- src/index.js
+++ src/index.js
@@ -1,59 +1,215 @@
-import { USER_NAME } from './utils/constants';
+import { reject } from 'lodash';
+import './js/module-11';
 
-import './styles/main.scss';
-import template from './templates/main.hbs';
-import data from './data/data.json';
-import {
-    setLocalStorage,
-    getLocalStorage,
-} from './utils/utils';
+// import { USER_NAME } from './utils/constants';
 
-import { throttle } from 'lodash';
+// import './styles/main.scss';
+// import template from './templates/main.hbs';
+// import data from './data/data.json';
+// import {
+//     setLocalStorage,
+//     getLocalStorage,
+// } from './utils/utils';
 
-let tmpUserNameState = '';
+// import { throttle } from 'lodash';
 
-window.onload = () => {
-    const container = document.getElementById('container');
+// let tmpUserNameState = '';
 
-    const isName = getLocalStorage();
+// window.onload = () => {
+//     const container = document.getElementById('container');
 
-    //Важно
+//     const isName = getLocalStorage();
 
-    // localStorage.setItem('test', JSON.stringify(data));
-    // try {
-    //     const test = localStorage.getItem('test');
-    //     const test2 = JSON.parse(test);
-    //     console.log(test2.navItems);
-    // } catch (error) {
-    //     console.log(error);
-    // }
+//     //Важно
 
-    if (isName) {
-        const newData = Object.assign({}, data, {
-            name: isName,
-        });
-        container.innerHTML = template(newData);
-    } else {
-        container.innerHTML = template(data);
+//     // localStorage.setItem('test', JSON.stringify(data));
+//     // try {
+//     //     const test = localStorage.getItem('test');
+//     //     const test2 = JSON.parse(test);
+//     //     console.log(test2.navItems);
+//     // } catch (error) {
+//     //     console.log(error);
+//     // }
 
-        const nameInput =
-            document.getElementById('nameInput');
-        const saveUser =
-            document.getElementById('saveUser');
+//     if (isName) {
+//         const newData = Object.assign({}, data, {
+//             name: isName,
+//         });
+//         container.innerHTML = template(newData);
+//     } else {
+//         container.innerHTML = template(data);
 
-        nameInput.addEventListener(
-            'input',
-            throttle(evt => {
-                tmpUserNameState = evt.target.value;
-            }, 250),
-        );
+//         const nameInput =
+//             document.getElementById('nameInput');
+//         const saveUser =
+//             document.getElementById('saveUser');
 
-        saveUser.addEventListener('click', () => {
-            setLocalStorage(tmpUserNameState);
-            const newData = Object.assign({}, data, {
-                name: tmpUserNameState,
-            });
-            container.innerHTML = template(newData);
-        });
-    }
-};
+//         nameInput.addEventListener(
+//             'input',
+//             throttle(evt => {
+//                 tmpUserNameState = evt.target.value;
+//             }, 250),
+//         );
+
+//         saveUser.addEventListener('click', () => {
+//             setLocalStorage(tmpUserNameState);
+//             const newData = Object.assign({}, data, {
+//                 name: tmpUserNameState,
+//             });
+//             container.innerHTML = template(newData);
+//         });
+//     }
+// };
+
+// Сеттеры Date
+// Для установки даты существует 2 способа. Установим 1500000000000мс
+// с начала эры Unix с помощью конструктора
+
+// const date = new Date(2017, 6, 14, 5, 40, 0, 0);
+// // проверим
+// console.log(
+//     `2017,6,14,5, 40 соотвествует ${date.getTime()}`,
+// );
+// /* С помощью методов set */
+// // Создадим новый объект с текущей датой
+
+// const secondDate = new Date();
+
+// // Изменим год и в нем же месяц и день месяца
+// secondDate.setFullYear(2017, 6, 14);
+// // Изменим час и в не же минуты, секунды и милесекунды
+// secondDate.setHours(5, 40, 0, 0);
+// // Проверим
+// console.log(
+//     `2017,6,14,5,40 соотвествует ${secondDate.getTime()}`,
+// );
+
+// Форматирование и вывод дат
+// let date = new Date();
+
+// // формат ввода
+// const options = {
+//     weekday: 'long',
+//     year: 'numeric',
+//     month: 'short',
+//     day: 'numeric',
+//     hour: '2-digit',
+//     minute: '2-digit',
+// };
+
+// // Украина
+// const localeUk = date.toLocaleDateString('UK-uk', options);
+
+// console.log(localeUk);
+
+// // USA
+// const localeUs = date.toLocaleDateString('en-US', options);
+// console.log(localeUs);
+
+// Promise API
+// const promise = new Promise((resolve, reject) => {
+//     /*
+//      * Эта функция будет вызвана автоматически. В ней можно выполнять
+//      * любые асинхронные операции. Когда они завершатся — нужно
+//      * вызвать одно из: resolve(результат) при успешном выполнении,
+//      * или reject(ошибка) при ошибке.
+//      */
+//     setTimeout(() => {
+//         resolve(console.log('success!'));
+//     }, 2000);
+// });
+
+// Promise and then
+
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         /*
+//          * Если какое-то условие выполняется, то есть все хорошо,
+//          * вызываем resolve и получает данные. Условие зависит от задачи.
+//          */
+//         resolve('Данные переданы в функцию resolve :)');
+//         // Если что-то не так, вызываем reject и передаем ошибку
+//         // reject("Данные переданны в функцию reject :(")
+//     }, 2000);
+// });
+// // Выполнится мгновенно
+// console.log('ПЕРЕД promise.then');
+
+// const onResolve = data => {
+//     console.log('ВНУТРИ promise.then - onResolve');
+//     console.log(data); // "Данные переданы в функцию resolve :)"
+// };
+
+// const onReject = error => {
+//     console.log('ВНУТРИ promise.then - onReject');
+//     console.log(error); // 'Ошибка передана в функцию reject :(
+// };
+
+// promise.then(
+//     // будет вызвана через 2 секунды, если обещание выполнится успешно
+//     onResolve,
+//     // будет вызвана через 2 секунды, если обещание выполнится с ошибкой
+//     onReject,
+// );
+
+// // Выполнится мгновенно
+// console.log('ПОСЛЕ promise.then');
+
+// Если onResolve и onReject не содержат сложной логики, их объявляют как инлайн функции в методе then.
+
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // Если все ок, то вызывается resolve и передаем данные
+//         resolve('Данные переданы в функцию resolve :)');
+//         // Если что-то не так, вызываем reject и передаем ошибку
+//         //         // reject("Данные переданны в функцию reject :(")
+//     }, 2000);
+// });
+// // Выполнится мгновенно
+// console.log('ПЕРЕД promise.then');
+
+// promise.then(
+//     // Будет вызвана через 2 секунды, если обещание выполнится успешно
+//     data => {
+//         console.log('ВНУТРИ promise.then ');
+//         console.log(data); // "Данные переданы в функцию resolve :)"
+//     },
+//     error => {
+//         console.log('ВНУТРИ promise.then ');
+//         console.log(error); // 'Ошибка передана в функцию reject :(
+//     },
+// );
+// // Выполнится мгновенно
+// console.log('ПОСЛЕ promise.then');
+
+// Promise and catch
+// Создадим обещание, сделаем задержку на 2 секунды, вызовем reject, имитируя выполнение промиса с ошибкой.
+
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject('Произошла Ошибка! :(');
+//     }, 2000);
+// });
+// /*
+//  * then не выполнится так как в функции fn, внутри new Promise(fn),
+//  * был вызван reject(). А catch как раз выполнится через 2 секунды
+//  */
+// promise
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     });
+
+// Promise adn finally
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('Успех!');
+//     }, 2000);
+// });
+
+// promise
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error))
+//     .finally(() => console.log('Финиш - finally()'));
