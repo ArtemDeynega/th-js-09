0edd22e580e86a6764a88a5546ee456ee251b04e
Artem Deynega <artemdeynega892@gmail.com>
2021-08-19 17:30:23 +0300

    1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,12 @@
+<<<<<<< Updated upstream
 export const USER_NAME = 'userName';
+=======
+export const USER_NAME = "userName";
+
+export const API_METHODS = {
+  GET: "GET",
+  POST: "POST",
+  PUT: "PUT",
+  DELETE: "DELETE"
+};
+>>>>>>> Stashed changes
